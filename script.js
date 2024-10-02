988a7aebb74c0da6f75a557fb02ce5c7cf4d2a07
satyabrata <satyabratamishra246@gmail.com>
2024-10-02 08:37:05 +0530

    rebuild

--- script.js
+++
@@ -1,9 +0,0 @@
-// Add event listeners to project links
-document.querySelectorAll('#projects a').forEach((link) => {
-    link.addEventListener('click', (event) => {
-        event.preventDefault();
-        const projectId = link.getAttribute('href').replace('#', '');
-        // Display project details
-        console.log(`Displaying project ${projectId} details`);
-    });
-});
